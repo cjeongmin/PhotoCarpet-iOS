932a2e7f75ecf016d8e4309a5759cd5870f627b4
cjeongmin <cjeongmin27@gmail.com>
2023-02-18 15:58:30 +0900

    Initial Commit

---
+++ PhotoCarpet/PhotoCarpetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCarpetApp.swift
+//  PhotoCarpet
+//
+//  Created by 최정민 on 2023/02/18.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCarpetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
